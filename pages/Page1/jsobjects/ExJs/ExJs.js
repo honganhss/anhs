729da0262e1bcba040816cfa4b120a4fb09cdd52
Hồng Ánh Nguyễn <honganh068204@gmail.com>
2024-11-01 11:01:37 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/ExJs/ExJs.js
@@ -0,0 +1,12 @@
+export default {
+	customerWidgetData(){
+		return{
+			"tips": [
+				"Pass data to this widget in the default model field",
+				"Custom business logic",
+				"Create events in the widget and trigger them in the javascript file using appsmith.triggerEvent('eventName')",
+				"Access data in CSS as var(--appsmith-model-{property-name})"
+			]
+		}
+	}
+}
